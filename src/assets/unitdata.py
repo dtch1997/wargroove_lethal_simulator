040cadec03eff080d1adc0733a6ab80bdda15563
Daniel Tan <dtch1997@stanford.edu>
2020-09-11 15:24:56 +0800

    Rename BaseDamageProvider; add move range and type

--- src/assets/unitdata.py
+++ src/assets/unitdata.py
@@ -1,50 +1,25 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Sep  5 00:12:36 2020
+Created on Fri Sep 11 15:02:57 2020
 
 @author: Daniel Tan
 """
 
-import json
 import numpy as np
+import json
+from src.utils import Index
 
-class Index:
-    """
-    A convenience class to convert hashable keys to indices
-    """
-    def __init__(self):
-        self.val2idx = {}
-        self.idx2val = []
+class UnitDataProvider:
     
-    def add(self, val):
-        next_idx = self.size
-        self.idx2val.append(val)
-        self.val2idx[val] = next_idx
-        
-    def get_index(self, val):
-        if val not in self.val2idx:
-            raise ValueError(val)
-        return self.val2idx[val]
-    
-    def get_value(self, idx):
-        if idx >= len(self.idx2val):
-            raise ValueError(idx)
-        return self.idx2val[idx]
-    
-    def values(self):
-        return self.val2idx.keys()
-        
-    @property
-    def size(self):
-        return len(self.idx2val)
-
-class BaseDamageProvider:
-    
-    def __init__(self, unit_index, damage_table, crit_table, counter_table):
+    def __init__(self, unit_index, movetype_index, 
+                 damage_table, crit_table, counter_table, movement_table):
         self.unit_index = unit_index
+        self.movetype_index = movetype_index
+        
         self.damage_table = damage_table
         self.crit_table = crit_table
         self.counter_table = counter_table
+        self.movement_table = movement_table
         
     @staticmethod
     def load(path):
@@ -56,13 +31,13 @@ class BaseDamageProvider:
             table_json = json.load(jsonfile)
         
         
-        unit_index = Index()
-        for i, unit in enumerate(table_json['soldier']['damage'].keys()):
-            unit_index.add(unit)
+        unit_index = Index(table_json['soldier']['damage'].keys())
+        movetype_index = Index([table_json[unit_name]['moveType'] for unit_name in unit_index.values])
             
         damage_table = np.ones(shape = (unit_index.size, unit_index.size), dtype=int) * -1
         crit_table = np.zeros(shape = unit_index.size, dtype=float)
         counter_table = np.zeros(shape = unit_index.size, dtype=bool)
+        movement_table = np.zeros(shape = [unit_index.size, 2])
         
         for atk_unit_name in unit_index.values():
             atk_idx = unit_index.get_index(atk_unit_name)
@@ -77,8 +52,12 @@ class BaseDamageProvider:
                 damage_table[atk_idx, def_idx] = dmg    
             crit_table[atk_idx] = unit_info['crit']
             counter_table[atk_idx] = unit_info['canCounter']
+            
+            movement_table[atk_idx, 0] = movetype_index.get_index(unit_info['moveType'])
+            movement_table[atk_idx, 1] = movetype_index.get_index(unit_info['moveRange'])
     
-        return BaseDamageProvider(unit_index, damage_table, crit_table, counter_table)
+        return UnitDataProvider(unit_index, movetype_index,
+                                damage_table, crit_table, counter_table, movement_table)
     
     def get_base_damage(self, atk_idx, def_idx) -> int:
         return self.damage_table[atk_idx, def_idx].item()
@@ -88,11 +67,9 @@ class BaseDamageProvider:
     
     def can_counter(self, unit_idx) -> bool:
         return self.counter_table[unit_idx].item()
-        
-        
-        
-if __name__ == "__main__":
-    table = BaseDamageProvider.load('damagetables/mindmg_2.0.json')
-
     
-        
+    def get_movement_type(self, unit_idx): 
+        return self.movement_table[unit_idx, 0]
+    
+    def get_movement_range(self, unit_idx):
+        return self.movement_table[unit_idx, 1]
